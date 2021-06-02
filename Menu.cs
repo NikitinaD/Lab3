901940157d4f256fcb2bcafe1d7bcdc416e63cfa
NikitinaD <dariya.nikitina1997@gmail.com>
2021-06-02 11:01:45 +0300

    conflict resolved

---
+++ Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashDesk
+{
+    class Menu
+    {
+        public static void m()
+        {
+            Console.WriteLine("Программа Касса в магазине");
+            while (true)
+            {
+                Autorization.Auto();
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1 - Прием поставки");
+                Console.WriteLine("2 - Информация о наличии товара на складе");
+                Console.WriteLine("3 - Возврат товара");
+                Console.WriteLine("4 - Продажа товара");
+                Console.WriteLine("0 - Выход");
+                string line = Console.ReadLine();
+                switch (line)
+                {
+                    case "1":
+                        Delivery.D();
+                        break;
+                    case "2":
+                        Search.S();
+                        break;
+                    case "3":
+                        Return.R();
+                        break;
+                    case "4":
+                        NewCheck.N();
+                        break;
+                    case "0": 
+                        Console.WriteLine("Программа завершила работу");
+                        Environment.Exit(0);
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
